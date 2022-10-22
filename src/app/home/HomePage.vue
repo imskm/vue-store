729ff445121d4ba9c2cfdec5464bf3e76f52798f
Shek Muktar <im.skm07@gmail.com>
2022-10-23 00:28:34 +0530

    HomePage.vue: Refactored and cleaned duplicate code

--- src/app/home/HomePage.vue
+++ src/app/home/HomePage.vue
@@ -8,131 +8,135 @@
           <div class="product">
 
 
-			<router-link :to="{name: 'product'}">
-	            <product-card />
-	        </router-link>
+            <div class="product-wrapper">
+        			<router-link :to="{name: 'product'}">
+                <product-card />
+    	        </router-link>
+            </div>
 
 
 
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-            <product-card />
-
-          
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/b.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/b.jpeg" alt="">
                 </div>
-                <div class="price">
-                  &#x20B9; 500
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/c.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/c.jpeg" alt="">
                 </div>
-                <div class="price">
-                  &#x20B9; 500
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/d.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/d.jpeg" alt="">
                 </div>
-                <div class="price">
-                  &#x20B9; 500
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          
+
           <div class="product">
             
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/e.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/e.jpeg" alt="">
                 </div>
-                <div class="price">
-                  &#x20B9; 500
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/f.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
-                </div>
-                <div class="price">
-                  &#x20B9; 500
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/g.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/f.jpeg" alt="">
                 </div>
-                <div class="price">
-                  &#x20B9; 500
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/h.jpeg" alt="">
-              </div>
-              <div class="card-text">
-                <div class="card-title">
-                  the fashionplus
-                  soild men hooded neck 
-                  <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/g.jpeg" alt="">
                 </div>
-                <div class="price">
-                  &#x20B9; 500
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="product-wrapper">
+              <div class="card">
+                <div class="card-image">
+                  <img src="assets/img/h.jpeg" alt="">
+                </div>
+                <div class="card-text">
+                  <div class="card-title">
+                    the fashionplus
+                    soild men hooded neck 
+                    <div class="brand-logo-2"><img src="assets/img/flipkart2.png" alt=""></div>
+                  </div>
+                  <div class="price">
+                    &#x20B9; 500
+                  </div>
                 </div>
               </div>
             </div>
